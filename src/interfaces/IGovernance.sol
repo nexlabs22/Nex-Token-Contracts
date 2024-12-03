1ff517e8e424df1a79b9a1bd31f64471f3dd2056
soheilprs <soheilparsaei19@protonmail.com>
2024-12-02 16:08:09 -0800

    Update IGovernance.sol

--- src/interfaces/IGovernance.sol
+++ src/interfaces/IGovernance.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.26;
 
 interface IGovernance {
     function getProposalStatus(uint256 proposalId) external view returns (bool approved);
+
+    function createProposal(
+        string calldata description,
+        address[] calldata targets,
+        uint256[] calldata values,
+        bytes[] calldata callDatas
+    ) external returns (uint256);
+
+    function isProposalApproved(uint256 proposalId) external view returns (bool);
 }
