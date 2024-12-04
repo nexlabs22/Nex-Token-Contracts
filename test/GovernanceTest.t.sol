9a5ca63a9da6035ed6cb9f01253bf51e6c13ef85
soheilprs <soheilparsaei19@protonmail.com>
2024-12-04 15:47:08 -0800

    Add more functions for test

--- test/GovernanceTest.t.sol
+++ test/GovernanceTest.t.sol
@@ -12,7 +12,10 @@ contract GovernanceTest is Test {
     address public owner = address(0x1);
     address public user1 = address(0x2);
     address public user2 = address(0x3);
+    address public user3 = address(0x5);
     address public vestingContract = address(0x4);
+
+    uint256 public proposalThreshold;
 
     function setUp() public {
         nexToken = new NEXToken();
@@ -26,17 +29,25 @@ contract GovernanceTest is Test {
         vm.prank(owner);
         nexToken.mint(user2, 400 * 10 ** 18);
 
+        vm.prank(owner);
+        nexToken.mint(user3, 600 * 10 ** 18);
+
         vm.prank(user1);
         nexToken.delegate(user1);
 
         vm.prank(user2);
         nexToken.delegate(user2);
 
+        vm.prank(user3);
+        nexToken.delegate(user3);
+
         vm.roll(block.number + 1);
 
         governance = new Governance();
+        proposalThreshold = 500 * 10 ** 18; // Set proposal threshold to 500 tokens
+
         vm.prank(owner);
-        governance.initialize(nexToken, 1 days);
+        governance.initialize(nexToken, 1 days, proposalThreshold);
     }
 
     function testProposalCreation() public {
@@ -53,6 +64,17 @@ contract GovernanceTest is Test {
         assertEq(proposal.description, "Proposal 1");
         assertEq(proposal.proposer, user1);
         assertEq(uint256(proposal.state), uint256(Governance.ProposalState.Active));
+    }
+
+    function testProposalCreationBelowThreshold() public {
+        vm.prank(user2);
+
+        address[] memory targets = new address[](1);
+        uint256[] memory values = new uint256[](1);
+        bytes[] memory callDatas = new bytes[](1);
+
+        vm.expectRevert("Insufficient voting power to create proposal");
+        governance.createProposal("Proposal 2", targets, values, callDatas);
     }
 
     function testExecuteProposalSuccess() public {
@@ -79,6 +101,9 @@ contract GovernanceTest is Test {
         vm.prank(user2);
         governance.vote(0, true);
 
+        vm.prank(user3);
+        governance.vote(0, true);
+
         vm.roll(proposal.endBlock + 1);
 
         vm.prank(user1);
@@ -98,6 +123,39 @@ contract GovernanceTest is Test {
 
         uint256 value = mockTarget.value();
         assertEq(value, 42);
+    }
+
+    function testMultiSigTimelockChange() public {
+        vm.prank(owner);
+        address[] memory approvers = new address[](3);
+        approvers[0] = user1;
+        approvers[1] = user2;
+        approvers[2] = user3;
+        governance.setApprovers(approvers);
+
+        vm.prank(user1);
+        governance.proposeTimelockDurationChange(2 days);
+
+        vm.prank(user2);
+        governance.proposeTimelockDurationChange(2 days);
+
+        uint256 newTimelockDuration = governance.timelockDuration();
+        assertEq(newTimelockDuration, 2 days);
+    }
+
+    function testMultiSigTimelockChangeInsufficientApprovals() public {
+        vm.prank(owner);
+        address[] memory approvers = new address[](3);
+        approvers[0] = user1;
+        approvers[1] = user2;
+        approvers[2] = user3;
+        governance.setApprovers(approvers);
+
+        vm.prank(user1);
+        governance.proposeTimelockDurationChange(2 days);
+
+        uint256 currentTimelockDuration = governance.timelockDuration();
+        assertEq(currentTimelockDuration, 1 days);
     }
 
     function sqrt(uint256 x) internal pure returns (uint256) {
